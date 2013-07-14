b6101ccb61985787ec5ec4203327d5d08351ee71
tiantian.gao <tiantian.gao@dianping.com>
2013-07-14 23:57:35 +0800

    添加cache service

---
+++ hachipets-web/src/main/java/com/gtt/pets/bean/CacheKeyHolder.java
@@ -0,0 +1,14 @@
+package com.gtt.pets.bean;
+
+/**
+ * 缓存key定义
+ * 
+ * @author tiantiangao
+ */
+public class CacheKeyHolder {
+
+	/**
+	 * 全局配置的缓存key
+	 */
+	public static final String GLOBAL_CONFIG = "oGlobalConfig";
+}
--- hachipets-web/src/main/java/com/gtt/pets/service/impl/GlobalServiceImpl.java
+++ hachipets-web/src/main/java/com/gtt/pets/service/impl/GlobalServiceImpl.java
@@ -1,10 +1,14 @@
 package com.gtt.pets.service.impl;
 
-import com.gtt.pets.dao.GlobalConfigDao;
-import com.gtt.pets.service.GlobalService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gtt.kenshin.cache.CacheKey;
+import com.gtt.kenshin.cache.CacheService;
+import com.gtt.pets.bean.CacheKeyHolder;
+import com.gtt.pets.dao.GlobalConfigDao;
+import com.gtt.pets.service.GlobalService;
 
 /**
  * 全局配置服务
@@ -16,6 +20,8 @@ public class GlobalServiceImpl extends BaseService implements GlobalService {
 
 	@Autowired
 	private GlobalConfigDao globalConfigDao;
+	@Autowired
+	private CacheService cacheService;
 
 	@Override
 	public String get(String name) {
@@ -23,6 +29,19 @@ public class GlobalServiceImpl extends BaseService implements GlobalService {
 			return null;
 		}
 
-		return globalConfigDao.get(name);
+		// load from cache
+		CacheKey cacheKey = new CacheKey(CacheKeyHolder.GLOBAL_CONFIG, name);
+		String config = cacheService.get(cacheKey);
+		if (null != config) {
+			return config;
+		}
+
+		// no cache, load from db
+		config = globalConfigDao.get(name);
+
+		// add cache
+		cacheService.add(cacheKey, null == config ? "" : config);
+
+		return config;
 	}
 }
