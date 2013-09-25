ce28698d16cf783e6d70d86fb7a95d42d7931162
tiantian.gao <tiantian.gao@dianping.com>
2013-09-25 17:44:31 +0800

    调整宠物电影样式

--- hachipets-web/src/main/webapp/WEB-INF/pages/media/mediaDetail.ftl
+++ hachipets-web/src/main/webapp/WEB-INF/pages/media/mediaDetail.ftl
@@ -9,7 +9,7 @@
     <div class="pets-movie-detail">
         <div class="pets-movie-return"><a href="<#if canReturnHistory>javascript:history.go(-1);<#else>/media</#if>">&lt;&lt; 返回上一页</a></div>
         <div class="pets-movie-title">
-            ${movie.name}<#if (movie.year)?? && movie.year gt 0><span>(${movie.year})</span></#if>
+            ${movie.name}<#if (movie.year)?? && movie.year gt 0> <span>(${movie.year})</span></#if>
         </div>
         <div class="pets-movie-desc">
             <div class="pets-movie-pic">
--- hachipets-web/src/main/webapp/js/petspic.js
+++ hachipets-web/src/main/webapp/js/petspic.js
@@ -7,10 +7,11 @@ jQuery.fn.loadpic = function(options){
         ml: 0
     }, options);
     var _self = this;
-    _self.hide();
+    _self.attr("src", options.src);
+//    _self.hide();
     var img = new Image();
     $(img).load(function(){
-        _self.attr("src", options.src);
+//        _self.attr("src", options.src);
         var wp = options.mw/_self.width();
         var hp = options.mh/_self.height();
         var originWidth = _self.width();
@@ -18,11 +19,11 @@ jQuery.fn.loadpic = function(options){
         if(wp < hp){
             _self.width(options.mw);
             _self.height(originHeight*wp);
-            _self.css('margin', ((options.mh - _self.height()) / 2 + parseInt(options.mt)) + 'px ' + (parseInt(options.ml) + parseInt(options.mw - _self.width()) * 0.5) + 'px').fadeIn('slow');
+            _self.css('margin', ((options.mh - _self.height()) / 2 + parseInt(options.mt)) + 'px ' + (parseInt(options.ml) + parseInt(options.mw - _self.width()) * 0.5) + 'px');
         }else{
             _self.width(originWidth*hp);
             _self.height(options.mh);
-            _self.css('margin', options.mt + 'px ' + (parseInt(options.ml) + parseInt(options.mw - _self.width()) * 0.5) + 'px').fadeIn('slow');
+            _self.css('margin', options.mt + 'px ' + (parseInt(options.ml) + parseInt(options.mw - _self.width()) * 0.5) + 'px');
         }
     }).attr("src", options.src);
     return _self;
