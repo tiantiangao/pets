ba80f7ca32ed4975e180dfedf9cf844f86f247be
tiantian.gao <tiantian.gao@dianping.com>
2013-10-11 10:33:09 +0800

    修改宠物详情页面

--- hachipets-web/src/main/java/com/gtt/pets/web/action/BaseAction.java
+++ hachipets-web/src/main/java/com/gtt/pets/web/action/BaseAction.java
@@ -15,111 +15,116 @@
  */
 package com.gtt.pets.web.action;
 
-import com.gtt.pets.service.GlobalService;
-import com.gtt.pets.web.exception.ProjectException;
-import com.opensymphony.xwork2.ActionSupport;
-import com.opensymphony.xwork2.Preparable;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.ParameterAware;
 import org.apache.struts2.interceptor.RequestAware;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+import com.gtt.kenshin.log.KenshinLogger;
+import com.gtt.kenshin.log.KenshinLoggerFactory;
+import com.gtt.pets.service.GlobalService;
+import com.gtt.pets.web.exception.ProjectException;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.Preparable;
 
 /**
  * Base Action
- *
+ * 
  * @author tiantiangao
  */
 public abstract class BaseAction extends ActionSupport implements RequestAware, ParameterAware, Preparable {
 
-    private static final long serialVersionUID = 1L;
-    protected Map<String, Object> request;
-    protected Map<String, String[]> parameters;
+	private static final long serialVersionUID = 1L;
+	private final KenshinLogger LOGGER = KenshinLoggerFactory.getLogger(getClass());
+	protected Map<String, Object> request;
+	protected Map<String, String[]> parameters;
+	@Autowired
+	private GlobalService globalService;
 
-    @Autowired
-    private GlobalService globalService;
+	@Override
+	public void prepare() throws Exception {
+		request.put("projectName", globalService.get("projectName"));
+		request.put("channel", "");
+	}
 
-    @Override
-    public void prepare() throws Exception {
-        request.put("projectName", globalService.get("projectName"));
-        request.put("channel", "");
-    }
+	protected void setChannel(String channel) {
+		request.put("channel", channel);
+	}
 
-    protected void setChannel(String channel) {
-        request.put("channel", channel);
-    }
+	@Override
+	public String execute() throws Exception {
+		try {
+			return doExecute();
+		} catch (Exception e) {
+			LOGGER.error("action executed error", e);
+			throw new ProjectException();
+		}
+	}
 
-    @Override
-    public String execute() throws Exception {
-        try {
-            return doExecute();
-        } catch (Exception e) {
-            throw new ProjectException();
-        }
-    }
+	protected abstract String doExecute() throws Exception;
 
-    protected abstract String doExecute() throws Exception;
+	@Override
+	public void setRequest(Map<String, Object> request) {
+		this.request = request;
+	}
 
-    @Override
-    public void setRequest(Map<String, Object> request) {
-        this.request = request;
-    }
+	@Override
+	public void setParameters(Map<String, String[]> parameters) {
+		this.parameters = parameters;
+	}
 
-    @Override
-    public void setParameters(Map<String, String[]> parameters) {
-        this.parameters = parameters;
-    }
+	public String getCookieValue(String cookieName) {
+		return getCookieValue(ServletActionContext.getRequest(), cookieName);
+	}
 
-    public String getCookieValue(String cookieName) {
-        return getCookieValue(ServletActionContext.getRequest(), cookieName);
-    }
+	public String getCookieValue(String domainName, String cookieName) {
+		return getCookieValue(ServletActionContext.getRequest(), domainName, cookieName);
+	}
 
-    public String getCookieValue(String domainName, String cookieName) {
-        return getCookieValue(ServletActionContext.getRequest(), domainName, cookieName);
-    }
+	private Cookie getCookie(HttpServletRequest request, String name) {
+		Cookie cookies[] = request.getCookies();
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				if (name.equals(cookies[i].getName())) {
+					return cookies[i];
+				}
+			}
+		}
+		return null;
+	}
 
-    private Cookie getCookie(HttpServletRequest request, String name) {
-        Cookie cookies[] = request.getCookies();
-        if (cookies != null) {
-            for (int i = 0; i < cookies.length; i++) {
-                if (name.equals(cookies[i].getName())) {
-                    return cookies[i];
-                }
-            }
-        }
-        return null;
-    }
+	private String getCookieValue(HttpServletRequest request, String name) {
+		Cookie cookie = getCookie(request, name);
+		return cookie != null ? cookie.getValue() : null;
+	}
 
-    private String getCookieValue(HttpServletRequest request, String name) {
-        Cookie cookie = getCookie(request, name);
-        return cookie != null ? cookie.getValue() : null;
-    }
+	private String getCookieValue(HttpServletRequest request, String domain, String name) {
+		Cookie cookie = getCookie(request, domain, name);
+		return cookie != null ? cookie.getValue() : null;
+	}
 
-    private String getCookieValue(HttpServletRequest request, String domain, String name) {
-        Cookie cookie = getCookie(request, domain, name);
-        return cookie != null ? cookie.getValue() : null;
-    }
-
-    private Cookie getCookie(HttpServletRequest request, String domain, String name) {
-        Cookie cookies[] = request.getCookies();
-        if (cookies == null)
-            return null;
-        if (domain == null || domain.equalsIgnoreCase("localhost")) {
-            for (Cookie cookie : cookies) {
-                if (name.equals(cookie.getName())) {
-                    return cookie;
-                }
-            }
-        }
-        for (Cookie cookie : cookies) {
-            if (domain.equals(cookie.getDomain()) && name.equals(cookie.getName())) {
-                return cookie;
-            }
-        }
-        return null;
-    }
+	private Cookie getCookie(HttpServletRequest request, String domain, String name) {
+		Cookie cookies[] = request.getCookies();
+		if (cookies == null)
+			return null;
+		if (domain == null || domain.equalsIgnoreCase("localhost")) {
+			for (Cookie cookie : cookies) {
+				if (name.equals(cookie.getName())) {
+					return cookie;
+				}
+			}
+		}
+		for (Cookie cookie : cookies) {
+			if (domain.equals(cookie.getDomain()) && name.equals(cookie.getName())) {
+				return cookie;
+			}
+		}
+		return null;
+	}
 
 }
--- hachipets-web/src/main/java/com/gtt/pets/web/action/baike/PetsBaikeDetailAction.java
+++ hachipets-web/src/main/java/com/gtt/pets/web/action/baike/PetsBaikeDetailAction.java
@@ -15,151 +15,158 @@
  */
 package com.gtt.pets.web.action.baike;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.gtt.pets.bean.baike.*;
 import com.gtt.pets.constants.CategoryType;
 import com.gtt.pets.constants.ChannelType;
 import com.gtt.pets.service.baike.PetsCategoryService;
 import com.gtt.pets.service.baike.PetsTypeService;
 import com.gtt.pets.web.action.BaseAction;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 宠物百科
- *
+ * 
  * @author tiantiangao
  */
 public class PetsBaikeDetailAction extends BaseAction {
-    private static final long serialVersionUID = 1L;
-    private static final String INDEX = "baike";
+	private static final long serialVersionUID = 1L;
+	private static final String INDEX = "baike";
+	@Autowired
+	private PetsCategoryService petsCategoryService;
+	@Autowired
+	private PetsTypeService petsTypeService;
+	// 输入
+	private int id;
+	// 输出
+	private PetsTypeDTO type;
+	private List<PetsCategoryDTO> categoryPath;
+	private List<PetsTypeAttrVO> attrList;
+	private List<PetsTypeFeatureVO> featureList;
 
-    @Autowired
-    private PetsCategoryService petsCategoryService;
-    @Autowired
-    private PetsTypeService petsTypeService;
+	@Override
+	public String doExecute() throws Exception {
+		setChannel(ChannelType.CHANNEL_BAIKE);
 
-    // 输入
-    private int id;
+		if (id < 1) {
+			return INDEX;
+		}
 
-    // 输出
-    private PetsTypeDTO type;
-    private List<PetsCategoryDTO> categoryPath;
-    private List<PetsTypeAttrVO> attrList;
-    private List<PetsTypeFeatureVO> featureList;
+		// 读取类型信息
+		type = petsTypeService.loadTypeByID(id);
+		if (type == null) {
+			return INDEX;
+		}
 
-    @Override
-    public String doExecute() throws Exception {
-        setChannel(ChannelType.CHANNEL_BAIKE);
+		// 读取分类路径信息
+		categoryPath = petsCategoryService.findPathByCategoryId(type.getCategoryId());
 
-        if (id < 1) {
-            return INDEX;
-        }
+		// 读取属性名映射表
+		Map<String, PetsTypeAttrNameDTO> attrNameMap = petsTypeService.loadTypeAttrNameMapByGroup(type
+				.getRootCategoryId());
+		attrNameMap.putAll(petsTypeService.loadTypeAttrNameMapByGroup(0));
 
-        // 读取类型信息
-        type = petsTypeService.loadTypeByID(id);
-        if (type == null) {
-            return INDEX;
-        }
+		attrList = new ArrayList<PetsTypeAttrVO>();
+		initBasicAttr(attrNameMap);
+		initExtraAttr(attrNameMap);
 
-        // 读取分类路径信息
-        categoryPath = petsCategoryService.findPathByCategoryId(type.getCategoryId());
+		return SUCCESS;
+	}
 
-        // 读取属性名映射表
-        Map<String, PetsTypeAttrNameDTO> attrNameMap = petsTypeService.loadTypeAttrNameMapByGroup(type.getRootCategoryId());
-        attrNameMap.putAll(petsTypeService.loadTypeAttrNameMapByGroup(0));
+	/**
+	 * 初始化扩展信息
+	 * 
+	 * @param attrNameMap
+	 */
+	private void initExtraAttr(Map<String, PetsTypeAttrNameDTO> attrNameMap) {
+		switch (type.getRootCategoryId()) {
+		case CategoryType.CATEGORY_DOG:
+			initDogAttr(attrNameMap);
+			break;
+		case CategoryType.CATEGORY_CAT:
+			break;
+		case CategoryType.CATEGORY_FISH:
+			break;
+		case CategoryType.CATEGORY_RABBIT:
+			break;
+		default:
+			break;
+		}
+	}
 
-        attrList = new ArrayList<PetsTypeAttrVO>();
-        initBasicAttr(attrNameMap);
-        initExtraAttr(attrNameMap);
+	/**
+	 * 初始化宠物狗扩展信息
+	 * 
+	 * @param attrNameMap
+	 */
+	private void initDogAttr(Map<String, PetsTypeAttrNameDTO> attrNameMap) {
+		PetsTypeDogDTO dog = petsTypeService.loadTypeDogByID(type.getId());
+		if (dog == null) {
+			return;
+		}
+		addAttr("dogFunction", show(dog.getFunction()), attrNameMap);
+		addAttr("dogHairLength", show(dog.getHairLength()), attrNameMap);
+		addAttr("dogHeight", show(dog.getHeight()), attrNameMap);
+		addAttr("dogWeight", show(dog.getWeight()), attrNameMap);
+		addAttr("dogLife", show(dog.getLife()), attrNameMap);
+		addAttr("dogColor", show(dog.getColor()), attrNameMap);
+	}
 
-        return SUCCESS;
-    }
+	private String show(String value) {
+		return StringUtils.isBlank(value) ? "暂无" : value;
+	}
 
-    /**
-     * 初始化扩展信息
-     *
-     * @param attrNameMap
-     */
-    private void initExtraAttr(Map<String, PetsTypeAttrNameDTO> attrNameMap) {
-        switch (type.getRootCategoryId()) {
-            case CategoryType.CATEGORY_DOG:
-                initDogAttr(attrNameMap);
-                break;
-            case CategoryType.CATEGORY_CAT:
-                break;
-            case CategoryType.CATEGORY_FISH:
-                break;
-            case CategoryType.CATEGORY_RABBIT:
-                break;
-            default:
-                break;
-        }
-    }
+	/**
+	 * 初始化公共的基本信息
+	 * 
+	 * @param attrNameMap
+	 */
+	private void initBasicAttr(Map<String, PetsTypeAttrNameDTO> attrNameMap) {
+		addAttr("name", show(type.getName()), attrNameMap);
+		addAttr("enName", show(type.getEnName()), attrNameMap);
+		addAttr("alias", show(type.getAlias()), attrNameMap);
+		addAttr("origin", show(type.getOrigin()), attrNameMap);
+		addAttr("type", categoryPath.get(categoryPath.size() - 1).getName(), attrNameMap);
+	}
 
-    /**
-     * 初始化宠物狗扩展信息
-     *
-     * @param attrNameMap
-     */
-    private void initDogAttr(Map<String, PetsTypeAttrNameDTO> attrNameMap) {
-        PetsTypeDogDTO dog = petsTypeService.loadTypeDogByID(type.getId());
-        addAttr("dogFunction", dog.getFunction(), attrNameMap);
-        addAttr("dogHairLength", dog.getHairLength(), attrNameMap);
-        addAttr("dogHeight", dog.getHeight(), attrNameMap);
-        addAttr("dogWeight", dog.getWeight(), attrNameMap);
-        addAttr("dogLife", dog.getLife(), attrNameMap);
-        addAttr("dogColor", dog.getColor(), attrNameMap);
-    }
+	/**
+	 * 添加属性至属性列表
+	 * 
+	 * @param name
+	 * @param value
+	 * @param attrNameMap
+	 */
+	private void addAttr(String name, String value, Map<String, PetsTypeAttrNameDTO> attrNameMap) {
+		PetsTypeAttrVO attrVO = new PetsTypeAttrVO();
+		PetsTypeAttrNameDTO attrNameDTO = attrNameMap.get(name);
+		attrVO.setName(attrNameDTO.getShowName());
+		attrVO.setValue(value);
+		attrVO.setSingleLine(attrNameDTO.getSingleLine() == 1);
+		attrList.add(attrVO);
+	}
 
-    /**
-     * 初始化公共的基本信息
-     *
-     * @param attrNameMap
-     */
-    private void initBasicAttr(Map<String, PetsTypeAttrNameDTO> attrNameMap) {
-        addAttr("name", type.getName(), attrNameMap);
-        addAttr("enName", type.getEnName(), attrNameMap);
-        addAttr("alias", type.getAlias(), attrNameMap);
-        addAttr("origin", type.getOrigin(), attrNameMap);
-        addAttr("type", categoryPath.get(categoryPath.size() - 1).getName(), attrNameMap);
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    /**
-     * 添加属性至属性列表
-     *
-     * @param name
-     * @param value
-     * @param attrNameMap
-     */
-    private void addAttr(String name, String value, Map<String, PetsTypeAttrNameDTO> attrNameMap) {
-        PetsTypeAttrVO attrVO = new PetsTypeAttrVO();
-        PetsTypeAttrNameDTO attrNameDTO = attrNameMap.get(name);
-        attrVO.setName(attrNameDTO.getShowName());
-        attrVO.setValue(value);
-        attrVO.setSingleLine(attrNameDTO.getSingleLine() == 1);
-        attrList.add(attrVO);
-    }
+	public PetsTypeDTO getType() {
+		return type;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public List<PetsCategoryDTO> getCategoryPath() {
+		return categoryPath;
+	}
 
-    public PetsTypeDTO getType() {
-        return type;
-    }
+	public List<PetsTypeAttrVO> getAttrList() {
+		return attrList;
+	}
 
-    public List<PetsCategoryDTO> getCategoryPath() {
-        return categoryPath;
-    }
-
-    public List<PetsTypeAttrVO> getAttrList() {
-        return attrList;
-    }
-
-    public List<PetsTypeFeatureVO> getFeatureList() {
-        return featureList;
-    }
+	public List<PetsTypeFeatureVO> getFeatureList() {
+		return featureList;
+	}
 }
