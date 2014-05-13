6a279d0efdae2b4651de0443ab7e02a1331d5277
tiantian.gao <gao12581@sina.com>
2014-05-13 22:21:16 +0800

    add loading

--- hachipets-web/src/main/webapp/js/petspic.js
+++ hachipets-web/src/main/webapp/js/petspic.js
@@ -7,11 +7,20 @@ jQuery.fn.loadpic = function(options){
         ml: 0
     }, options);
     var _self = this;
+    var container = $("<div></div>");
+    container.width(options.mw);
+    container.height(options.mh);
+    container.css("text-align", "center");
+    _self.wrap(container);
+    var loading = $("<img width=\"32\" height=\"32\" alt=\"加载中...\" title=\"图片加载中...\" src=\"http://hachipets1.qiniudn.com/s/img/loading.gif\" />");
+    loading.width(32);
+    loading.height(32);
+    loading.css("margin-top", (options.mh-32)/2+"px");
+    $(_self).after(loading);  //添加loading图片
+    _self.hide();
     _self.attr("src", options.src);
-//    _self.hide();
     var img = new Image();
     $(img).load(function(){
-//        _self.attr("src", options.src);
         var wp = options.mw/_self.width();
         var hp = options.mh/_self.height();
         var originWidth = _self.width();
@@ -25,6 +34,8 @@ jQuery.fn.loadpic = function(options){
             _self.height(options.mh);
             _self.css('margin', options.mt + 'px ' + (parseInt(options.ml) + parseInt(options.mw - _self.width()) * 0.5) + 'px');
         }
+        loading.remove();
+        _self.show();
     }).attr("src", options.src);
     return _self;
 }
